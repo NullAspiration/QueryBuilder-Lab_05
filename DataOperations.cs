739dc848a7373b1f2c90644dafea922f08eef2c3
69oo69 <53282201+69oo69@users.noreply.github.com>
2022-11-01 23:44:44 -0400

    ...

--- DataOperations.cs
+++ DataOperations.cs
@@ -1,47 +1,50 @@
 ﻿using Microsoft.Data.Sqlite;
-
-public class DataOperations : IDisposable
+namespace QueryBuilder
 {
-    private readonly SqliteConnection connection;
-    /// <summary>
-    /// Opens connenction to DB upon creation of object
-    /// </summary>
-    public DataOperations(string dbPath)//path to db file 
+
+    public class DataOperations : IDisposable
     {
-        connection = new SqliteConnection(dbPath);
-        connection.Open();
-    }
-
-    /// <summary>
-    /// Reads a row from db, returns obj of type T
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="id">ID of record to read</param>
-    /// <returns></returns>
-    public string ReadAll<T>() where T : new()
-    {
-        var dataString = "";
-        //create command
-        var command = connection.CreateCommand();
-
-        //Set command text
-        command.CommandText = $"SELECT * FROM {typeof(T).Name}";
-
-        //reader for database
-        var reader = command.ExecuteReader();
-
-        while (reader.Read())
+        private readonly SqliteConnection connection;
+        /// <summary>
+        /// Opens connenction to DB upon creation of object
+        /// </summary>
+        public DataOperations(string dbPath)//path to db file 
         {
-            dataString += typeof(T).Name + " " + reader.GetString(0) + "\n";
+            connection = new SqliteConnection(dbPath);
+            connection.Open();
         }
 
-        return dataString;
-    }
+        /// <summary>
+        /// Reads a row from db, returns obj of type T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id">ID of record to read</param>
+        /// <returns></returns>
+        public string ReadAll<T>() where T : new()
+        {
+            var dataString = "";
+            //create command
+            var command = connection.CreateCommand();
 
-    //implements IDisposable
-    public void Dispose()
-    {
-        //closes connection to SqliteDB
-        connection.Close();
+            //Set command text
+            command.CommandText = $"SELECT * FROM {typeof(T).Name}";
+
+            //reader for database
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                dataString += typeof(T).Name + " " + reader.GetString(0)  + "\n";
+            }
+
+            return dataString;
+        }
+
+        //implements IDisposable
+        public void Dispose()
+        {
+            //closes connection to SqliteDB
+            connection.Close();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,28 +2,70 @@
 {
     public class Program
     {
-        static string CONNECTION_STRING = "Data Source = " + ProjectRoot.Root + Path.DirectorySeparatorChar + "Data" + Path.DirectorySeparatorChar + "QueryBuilderDB.db";
-        static void Main(String[] args)
+        static readonly string CONNECTION_STRING = "Data Source = " + ProjectRoot.Root + Path.DirectorySeparatorChar + "Data" + Path.DirectorySeparatorChar + "QueryBuilderDB.db";
+        static void Main(string[] args)
         {
-            //Helper uses IDisposeable, Dispose() is automatically called (so using is used)
-            using (var dbOp = new DataOperations(CONNECTION_STRING))
+            if (args is null)
             {
-                
-                Console.WriteLine("|__________Reading Users____________________|" + "\n");
-                Console.WriteLine(dbOp.ReadAll<Users>());
-               
-                Console.WriteLine("|__________Reading Books____________________|" + "\n");
-                Console.WriteLine(dbOp.ReadAll<Books>());
-                
-                Console.WriteLine("|__________Reading Author___________________|" + "\n");
-                Console.WriteLine(dbOp.ReadAll<Author>());
-                
-                Console.WriteLine("|__________Reading Categories_______________|" + "\n");
-                Console.WriteLine(dbOp.ReadAll<Categories>());
-                
-                Console.WriteLine("|__________Reading BooksOutOnLoan___________|" + "\n");
-                Console.WriteLine(dbOp.ReadAll<BooksOutOnLoan>());
+                throw new ArgumentNullException(nameof(args));
             }
+
+
+            //DataOPs uses IDisposeable, Dispose() is automatically called (so using is used)
+            using var dbOp = new DataOperations(CONNECTION_STRING);
+            Console.WriteLine("|__________Reading Users.csv________________|" + "\n");
+            var userlist = ReadUsersFromCSV();
+            userlist.Sort();
+            foreach (var user in userlist)
+            {
+                Console.WriteLine(user);
+                Console.WriteLine("-----------------");
+            }
+
+
+
+            Console.WriteLine("|__________Reading Users____________________|" + "\n");
+            Console.WriteLine(dbOp.ReadAll<Users>());
+
+            Console.WriteLine("|__________Reading Books____________________|" + "\n");
+            Console.WriteLine(dbOp.ReadAll<Books>());
+
+            Console.WriteLine("|__________Reading Author___________________|" + "\n");
+            Console.WriteLine(dbOp.ReadAll<Author>());
+
+            Console.WriteLine("|__________Reading Categories_______________|" + "\n");
+            Console.WriteLine(dbOp.ReadAll<Categories>());
+
+            Console.WriteLine("|__________Reading BooksOutOnLoan___________|" + "\n");
+            Console.WriteLine(dbOp.ReadAll<BooksOutOnLoan>());
+        }
+        public static List<Users> ReadUsersFromCSV()
+        {
+            Console.WriteLine("Source Dir: " + ProjectRoot.Root);
+            var userlist = new List<Users>();
+            using (var reader = new StreamReader(ProjectRoot.Root + Path.DirectorySeparatorChar + "Data" + Path.DirectorySeparatorChar + "Users.csv"))
+            {
+                try
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        // read line by line
+                        var line = reader.ReadLine();
+
+                        // split the string on the delimiter (,)
+                        var values = line.Split(",");
+
+
+                        userlist.Add(new Users(int.Parse(values[0]), values[1], values[2], values[3], values[4], values[5], values[6]));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            } // end using
+
+            return userlist;
         }
     }
 }
